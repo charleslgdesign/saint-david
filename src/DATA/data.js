9aec2a5cd21e0d33651aa18ee5f4563d8963ca81
Charlie64N7 <79482179+Charlie64N7@users.noreply.github.com>
2022-03-09 14:39:49 +1300

    u

---
+++ src/DATA/data.js
@@ -0,0 +1,93 @@
+ const data = [
+    {
+    id:1,
+    category: 'T-Shirt',
+    image: '/IMG/10.jpg',
+    bside:'/IMG/1.jpg'
+    },   
+
+    {
+        id:2,
+        category: 'Jacket',
+        image: '/IMG/5.jpg',
+        bside:'/img/2.jpg'
+        }, 
+
+        {
+            id:3,
+            category: 'T-Shirt',
+            image: '/IMG/12.jpg',
+            bside: '/IMG/3.jpg'
+            }, 
+
+            {
+                id:4,
+                category: 'Long Sleeve Button-Up Shirt',
+                image: '/IMG/4.jpg',
+                bside: '/IMG/11.jpg'
+                }, 
+
+     
+                    {
+                        id:5,
+                        image: '/IMG/6.jpg',
+                        bside: 'IMG/19.jpg'
+
+                        }, 
+
+                        {
+                            id:6,
+                            image: '/IMG/24.jpg',
+                            bside: 'IMG/7.jpg'
+                            }, 
+
+                            {
+                                id:8,
+                                image: '/IMG/18.jpg',
+                                bside: '/IMG/8.jpg'
+                                }, 
+
+                                {
+                                    id:9,
+                                    image: '/IMG/9.jpg',
+                                    bside: '/IMG/17.jpg'
+                                    }, 
+
+
+                     
+
+                                            {
+                                                    id:13,
+                                                    image: '/IMG/14.jpg',
+                                                    bside: '/IMG/13.jpg'
+                                                    },
+
+
+                                                        {
+                                                            id:15,
+                                                            image: '/IMG/15.jpg',
+                                                            bside: '/IMG/16.jpg'
+
+                                                            },
+
+                                                     
+                                                                {
+
+                                                                id:20,
+                                                                image: '/IMG/20.jpg',
+                                                                bside: '/IMG/22.jpg'
+                                                                
+                                                                },
+
+                                                                                {
+
+                                                                                    id:21,
+                                                                                    image: '/IMG/21.jpg',
+                                                                                    bside: '/IMG/23.jpg'
+                                                                                    },
+
+                               
+                    
+]
+
+export default data
---
+++ src/product.js
@@ -0,0 +1,41 @@
+import { React, useState } from 'react'
+import data from './DATA/data.js'
+
+
+
+export default function Product(props) {
+    const { product } = props
+
+    const [productImage, setProductImage] = useState(product.image)
+    const [flip, setFlip] = useState(true)
+
+    const toggleSide = (e) => {
+
+      if (flip === true) {
+        setProductImage(product.bside)
+      } else {
+        setProductImage(product.image)
+      }
+    }
+
+    
+
+  return (
+
+  
+
+    <div>
+      {/* {showProduct?<div>
+        <img src={activeImage}/>
+        <button onClick={()=>setShowProduct(!product)}>back</button></div>:null} */}
+    <img class='product-image'  
+    src={productImage} tag={product.id} id={product.id}
+    onClick={(e)=>{setFlip(!flip);toggleSide(e)}}
+    // onClick={(e)=>ShowProduct(e)}
+    />
+    <div class='label'>
+    </div>
+    </div>
+  )
+}
+
