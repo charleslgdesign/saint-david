6c3fb6dca0d0356271843601ba513ab81b20dd06
Charlie64N7 <79482179+Charlie64N7@users.noreply.github.com>
2022-03-09 14:41:04 +1300

    dfsf
    yup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
+import react from 'react'
 import './App.css';
+import data from './DATA/data.js'
+import Product from './product.js'
+
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  <Products/>
+  </>
+  )
+
+}
+
+function Products() {
+
+
+  return (
+
+
+  <div class='products-container'>
+  {data.map((i)=>(
+    <>
+  <Product product={i} ></Product></>
+))}
+  </div>
+
+)
+  
 }
 
 export default App;
